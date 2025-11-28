c25c3a92df330fb096382e23c363bafb3d994160
Joshua Strickland <josh.strickland@hotmail.com>
2025-11-27 21:43:04 -0330

    Add make_cards function & draw_card function

--- CP1856_Project_Jstrickland.py
+++ CP1856_Project_Jstrickland.py
@@ -2,23 +2,72 @@ import random
 
 def wager():
     player_money = 100.00
-    while True
-        player_wager = float(input("How much would you like to bet?: ")
-            if player_wager > player_money:
-                print("Insufficient funds. Please try again.")
-            elif player_wager <= 0:
-                print("Enter a number greater than 0. Please try again.")
-            else:
-                player_money -= player_wager
-                break
+    while True:
+        player_wager = float(input("How much would you like to bet?: "))
+        if player_wager > player_money:
+            print("Insufficient funds. Please try again.")
+        elif player_wager <= 0:
+            print("Enter a number greater than 0. Please try again.")
+        else:
+            player_money -= player_wager
+            break    
 
-def cards():
-    suit = ["Diamonds", "Hearts", "Clubs", "Spades"]
-    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-    value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "1"]
+def make_cards():
+    suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    deck = []
+    for suit in suits:
+        for rank in ranks:
+            if rank == "2":
+                value = 2
+            elif rank == "3":
+                value = 3
+            elif rank == "4":
+                value = 4
+            elif rank == "5":
+                value = 5
+            elif rank == "6":
+                value = 6
+            elif rank == "7":
+                value = 7
+            elif rank == "8":
+                value = 8
+            elif rank == "9":
+                value = 9
+            elif rank == "10":
+                value = 10
+            elif rank == "Jack":
+                value = 10
+            elif rank == "Queen":
+                value = 10
+            elif rank == "King":
+                value = 10
+            elif rank == "Ace":
+                value = 1
+            card = [suit, rank, value]
+            deck.append(card)
+    return deck
 
+def draw_card(deck, player_hand):
+    draw = random.randint(0, len(deck))
+    drawn_card = deck[draw]
+    player_hand.append(drawn_card)
+    deck.pop(draw)
+    
 def main():
+    player_hand = []
+    dealer_hand = []
     print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+
+    deck = make_cards()
+    print(deck)
+    draw_card(deck, player_hand)
+    draw_card(deck, player_hand)
+    draw_card(deck, player_hand)
+    draw_card(deck, player_hand)
+    draw_card(deck, player_hand)
+    print(f"Player Hand: {player_hand}")
 
 if __name__ == "__main__":
     main()
